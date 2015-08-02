9474b3672e65f29507becfd9911cbd5de750b4f7
hexavis <ILHx2aLm>
2015-08-02 15:17:13 -0400

    messing around with scrollbars

--- HeroHWTracker/Default.Master.designer.cs
+++ HeroHWTracker/Default.Master.designer.cs
@@ -121,15 +121,6 @@ namespace HeroHWTracker {
         protected global::System.Web.UI.WebControls.Label showLevelUp;
         
         /// <summary>
-        /// panel1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel panel1;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
