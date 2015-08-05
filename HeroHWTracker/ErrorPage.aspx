68a29475d3ec48e476a168ac4e414687f503c653
hexavis <ILHx2aLm>
2015-08-05 13:53:47 -0400

    Files that didn't seem to be pushed.

---
+++ HeroHWTracker/ErrorPage.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="Error 404" Language="C#" MasterPageFile="~/Default.Master" AutoEventWireup="true" CodeBehind="ErrorPage.aspx.cs" Inherits="HeroHWTracker.Error404" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Panel ID="reqId" runat="server" HorizontalAlign="center">
+        <div class="containter-fluid">
+            <div class="col-md-6 col-md-offset-3 col-centered">
+                <div class="well">
+
+                    <h2>Error:</h2>
+                    <p></p>
+                    <asp:Label ID="FriendlyErrorMsg" runat="server" Text="Label" Font-Size="Large" Style="color: red"></asp:Label>
+
+                    <asp:Panel ID="DetailedErrorPanel" runat="server" Visible="false">
+                        <p>&nbsp;</p>
+                        <h4>Detailed Error:</h4>
+                        <p>
+                            <asp:Label ID="ErrorDetailedMsg" runat="server" Font-Size="Small" /><br />
+                        </p>
+
+                        <h4>Error Handler:</h4>
+                        <p>
+                            <asp:Label ID="ErrorHandler" runat="server" Font-Size="Small" /><br />
+                        </p>
+
+                        <h4>Detailed Error Message:</h4>
+                        <p>
+                            <asp:Label ID="InnerMessage" runat="server" Font-Size="Small" /><br />
+                        </p>
+                        <p>
+                            <asp:Label ID="InnerTrace" runat="server" />
+                        </p>
+                    </asp:Panel>
+                    <asp:Label runat="server" ID="ErrorMsgTextBox"></asp:Label>
+                    <img alt="Brand" src="<%=ResolveClientUrl("~/img/j0Bee17.jpg")%>">
+                </div>
+                <!-- well -->
+            </div>
+        </div>
+    </asp:Panel>
+</asp:Content>
---
+++ HeroHWTracker/HomeWork/Complete.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Entity;
+using HeroHWTracker.Models;
+using System.Data.SqlClient;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using System.Collections.Generic;
+
+
+namespace HeroHWTracker.HomeWork
+{
+    public partial class Complete : System.Web.UI.Page
+    {
+		protected HeroHWTracker.Models.HeroEntities _db = new HeroHWTracker.Models.HeroEntities();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("~/login.aspx");
+            }
+            else
+            {
+
+            }
+        }
+
+        // Model binding method to get List of HomeWork entries
+        // USAGE: <asp:ListView SelectMethod="GetData">
+        public IQueryable<HeroHWTracker.Models.HomeWork> GetData()
+        {
+            return _db.HomeWork;
+        }
+
+         public IQueryable<HeroHWTracker.Models.HomeWork> Index()
+    {
+             //Grab the hero image, exp and homework in progress and get them ready.
+                    string currentUser = HttpContext.Current.User.Identity.GetUserName();
+            int userInfoID = 0;
+
+            //make a connection to the database
+            string cs = "Data Source=pbc2o8qyql.database.windows.net;Initial Catalog=Hero;User ID=apple;Password=skull!1223;Trusted_Connection=False;Encrypt=True;";
+            SqlConnection conn = new SqlConnection(cs);
+
+            //grab hero level, gender and exp
+            SqlCommand check = new SqlCommand("SELECT * FROM [UserInfo] where [Username]=@username", conn);
+            check.Parameters.AddWithValue("@username", currentUser);
+
+            try
+            {
+                conn.Open();
+                //open and read the information in the database
+                using (SqlDataReader reader = check.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        userInfoID = (int)reader["UserInfoID"];
+
+                    }
+
+                }
+                conn.Close();
+            }
+            catch (Exception ex1)
+            {
+                conn.Close();
+            }
+
+            return _db.HomeWork.Where(a => a.UserInfoID == userInfoID && a.isComplete == 1);
+    }
+
+        protected void CompleteB_Click(object sender, EventArgs e)
+        {
+
+            Response.Redirect("~/HomeWork/Default.aspx");
+        }
+
+    
+
+    }
+}
+
